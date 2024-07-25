e751757b1ae8958c436b755f7763a3bda30da75f
Michael T <bravmech@gmail.com>
2024-07-25 09:19:09 +0200

    chore: Remove line_profiler from IPython extensions

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -37,7 +37,7 @@ c.InteractiveShellApp.exec_lines = [
 ]
 
 ## A list of dotted module names of IPython extensions to load.
-c.InteractiveShellApp.extensions = ['autoreload', 'line_profiler']
+c.InteractiveShellApp.extensions = ['autoreload']
 
 ## dotted module name of an IPython extension to load.
 # c.InteractiveShellApp.extra_extension = ''
