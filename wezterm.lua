67a79bf8401e9cef350af89260534be91d162568
Michael T <bravmech@gmail.com>
2025-01-05 13:20:18 +0100

    Update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -6,4 +6,17 @@ config.font_size = 14.5
 config.default_prog = { '/opt/homebrew/bin/fish' }
 config.tab_bar_at_bottom = true
 
+config.keys = {
+  {
+    mods = "CMD",
+    key = "d",
+    action = wezterm.action.SendKey { mods = "CTRL", key = "d" },
+  },
+  {
+    mods = "CMD",
+    key = "c",
+    action = wezterm.action.SendKey { mods = "CTRL", key = "c" },
+  },
+}
+
 return config
