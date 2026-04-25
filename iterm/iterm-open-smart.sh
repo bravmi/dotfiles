23dca6bc77375483cdf31a000cec87a7173e1d0d
Michael T <bravmech@gmail.com>
2026-04-25 10:02:13 +0400

    Update iterm open smart

--- iterm/iterm-open-smart.sh
+++ iterm/iterm-open-smart.sh
@@ -3,14 +3,15 @@
 file="$1"
 line="$2"
 
-# If it's a Python file, open in PyCharm at the given line
+# Fallback if line is empty or not numeric
+if ! [[ "$line" =~ ^[0-9]+$ ]]; then
+  line=1
+fi
+
 if [[ "$file" == *.py ]]; then
-  # Fallback if line is empty or not numeric
-  if ! [[ "$line" =~ ^[0-9]+$ ]]; then
-    line=1
-  fi
   /opt/homebrew/bin/pycharm --line "$line" "$file"
+elif [[ "$file" == *.go ]]; then
+  /opt/homebrew/bin/goland --line "$line" "$file"
 else
-  # Non-Python files -> open with default app
   open "$file"
 fi
